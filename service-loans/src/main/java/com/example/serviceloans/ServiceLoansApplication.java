388534d665faecd2b3b3340e313d166a434894e0
duytc <duytc@twendeesoft.com>
2024-02-25 14:42:42 +0700

    created: service loans

---
+++ service-loans/src/main/java/com/example/serviceloans/ServiceLoansApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceLoansApplication.class, args);
+    }
+
+}
---
+++ service-loans/src/test/java/com/example/serviceloans/ServiceLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceLoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
