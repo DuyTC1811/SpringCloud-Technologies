cba9d1db0e36391383a8f26b0a3ea11b15aa2f7f
duytc <duytc@twendeesoft.com>
2024-02-25 12:13:59 +0700

    created: Service Gaterway

---
+++ service-gaterway/src/main/java/com/example/servicegaterway/ServiceGaterwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicegaterway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGaterwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGaterwayApplication.class, args);
+    }
+
+}
---
+++ service-gaterway/src/test/java/com/example/servicegaterway/ServiceGaterwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicegaterway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGaterwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
