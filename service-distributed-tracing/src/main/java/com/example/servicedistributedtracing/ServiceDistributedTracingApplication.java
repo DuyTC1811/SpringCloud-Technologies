7affba545f3d436ce95bf9b8567af188ec9caff7
duytc <duytc@twendeesoft.com>
2024-02-25 12:13:27 +0700

    created: Service Distributed

---
+++ service-distributed-tracing/src/main/java/com/example/servicedistributedtracing/ServiceDistributedTracingApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicedistributedtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDistributedTracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDistributedTracingApplication.class, args);
+    }
+
+}
---
+++ service-distributed-tracing/src/test/java/com/example/servicedistributedtracing/ServiceDistributedTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicedistributedtracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDistributedTracingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
